04280c609b5d7a36d77d819808223f622bb6b078
Ayomide Adefe <33925773+Ayomide96@users.noreply.github.com>
2023-06-20 01:04:47 +0100

    Update and rename Jaam-element.js to NAC-element.js

--- NAC-element.js
+++ NAC-element.js
@@ -10,13 +10,17 @@ export class EditableRepeatingSection extends LitElement {
         this.who = 'World';
       }						
                                 
-      render() {
+      render() {       
+var numOfClicks = document.documentElement.querySelectorAll('[formcontrolid="fc_1d2c3c8e8556462fb5107b35b9785e23"] .ntx-repeating-section-repeated-section').length - 1;
+for(var i = 0, j = numOfClicks; i < j; i++) {document.documentElement.querySelector('[formcontrolid="fc_f7edea3c7bbc474ba19424f8e5269e62"] .btn-repeating-section-new-row').click()
+}
+var readOnlyTextShort = document.documentElement.querySelectorAll('[formcontrolid="fc_1d2c3c8e8556462fb5107b35b9785e23"] input:not([type="checkbox"])');
+readOnlyTextShort.forEach(function (input) { document.documentElement.querySelector(`[formcontrolid="fc_f7edea3c7bbc474ba19424f8e5269e62"] [aria-label="${input.ariaLabel}"]`).value = input.value; })
         return html`<p>Hello ${this.who}<p/>`;
       }
-
       static getMetaConfig() {
         return {
-          controlName: 'Ayo Test Section',
+          controlName: 'NACPlug',
           fallbackDisableSubmit: false,
           version: '1.0',
           properties: {
